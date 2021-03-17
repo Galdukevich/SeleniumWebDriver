af3ba69ba4dc10a49a823ff801538f9b4a6b3ae7
Alexander Galdukevich <goldychio@gmail.com>
2021-03-17 11:13:48 -0700

    TutByLoginTest and MultiSelectListTest

---
+++ SeleniumWebDriver/SeleniumWebDriver/TutByLoginTest/Pages/MainPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumWebDriver.TutByLoginTest.Pages
+{
+    public class MainPage
+    {
+        private const string _userNamePanelByClass = "uname";
+
+        private IWebDriver _driver;
+        IWebElement _UserNamePanel;
+
+        public MainPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public string GetUserName(ref string username)
+        {
+            //explicit waite
+            WebDriverWait wait = new WebDriverWait(_driver, new System.TimeSpan(5000));
+            wait.PollingInterval = new System.TimeSpan(2500);            
+            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName(_userNamePanelByClass)));
+
+            _UserNamePanel = _driver.FindElement(By.ClassName(_userNamePanelByClass));
+            username = _UserNamePanel.Text;
+            return username;
+        }
+    }
+}
---
+++ SeleniumWebDriver/SeleniumWebDriver/TutByLoginTest/Tests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using SeleniumWebDriver.TutByLoginTest.Pages;
+
+namespace SeleniumWebDriver.TutByLoginTest
+{
+    [TestFixture]
+    public class WebDriverTests
+    {
+        [TestCase("seleniumtests@tut.by", "123456789zxcvbn", "Selenium Test", "")]
+        [TestCase("seleniumtests2@tut.by", "123456789zxcvbn", "Selenium Test", "")]
+
+        public void TestMethod(string login, string password, string username, string loggedInUser)
+        {
+            IWebDriver _driver = new FirefoxDriver();
+         
+            //create new login page
+            LoginPage loginPage = new LoginPage(_driver);
+
+            //enter login and password
+            loginPage.LogIn(login, password);
+
+            //go to MainPage
+            MainPage mainPage = loginPage.OpenMainPage();
+
+            //get logged in username
+            mainPage.GetUserName(ref loggedInUser);
+
+            Assert.AreEqual(username, loggedInUser);
+
+            _driver.Close();
+        }
+    }
+}
