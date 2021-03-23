16a809d412f20e95f8fbb61a19a5f2ae1b69a1aa
Alexander Galdukevich <goldychio@gmail.com>
2021-03-23 05:21:08 -0700

    Revert "commit"
    This reverts commit eee117a0805e0b749db6db5f5c5c0b0b5c4c2105.

--- SeleniumWebDriver/SeleniumWebDriver/Tests.cs
+++ SeleniumWebDriver/SeleniumWebDriver/Tests.cs
@@ -1,59 +1,18 @@
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
-using SeleniumWebDriver.Pages;
-using System;
 
-namespace SeleniumWebDriver
+namespace Tests
 {
-    [TestFixture]
     public class Tests
     {
-        IWebDriver _driver;
-        WebDriverWait wait;
-
         [SetUp]
-        public void PreparingTest()
+        public void Setup()
         {
-            _driver = new FirefoxDriver();
-            wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(3));
         }
 
-        [TearDown]
-        public void ReleaseTest()
+        [Test]
+        public void Test1()
         {
-            _driver.Close();
+            Assert.Pass();
         }
-
-        [TestCase("seleniumtests@tut.by", "123456789zxcvbn", "Selenium Test", "")]
-
-        public void TestMethod(string login, string password, string username, string loggedInUser)
-        {
-
-            //create login page and initialize WebElements
-            LoginPage loginPage = new LoginPage(_driver);
-
-            //enter login and password and sign in
-            loginPage.LogIn(login, password);
-
-            //create main page and initialize WebElements
-            MainPage mainPage = loginPage.OpenMainPage();
-
-            //get logged in username
-            mainPage.GetUserName(ref loggedInUser);
-
-            //make sure that user is logged in
-            Assert.AreEqual(username, loggedInUser);
-
-            //logout
-            mainPage.LogOut();
-
-            //create LoginPage again, after LogOut
-            LoginPage logOutPage = mainPage.OpenLoginPage();
-
-            //make sure that user is logged out
-            Assert.IsTrue(logOutPage.EnterButtonIsDisplayed());         
-        }        
     }
 }
